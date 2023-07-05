869a37ce34de8cbd861e69ce32c54692527ee7bc
Mallory Hayr <hey@igalaxy.dev>
2023-07-05 12:57:51 -0700

    add use-lanyard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
+import { useLanyard } from 'use-lanyard';
+
+import Image from 'next/image';
+
+const DISCORD_ID = '182292736790102017';
+
 export default function Home() {
+	const { data } = useLanyard(DISCORD_ID);
+
 	return (
 		<main>
 			<a href="https://igalaxy.dev">test</a>
+			{data && data.discord_user && data.discord_user.avatar ? (
+				<Image
+					src={`https://cdn.discordapp.com/avatars/${DISCORD_ID}/${data.discord_user.avatar}.png`}
+					alt={'avatar'}
+					width={64}
+					height={64}
+				/>
+			) : (
+				<></>
+			)}
 		</main>
 	);
 }
