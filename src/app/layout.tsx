3c0e446508092d2074d6efb3f2fc5866305d1ac5
Mallory Hayr <hey@igalaxy.dev>
2023-07-05 03:39:12 -0700

    misc: replace title with hostname (real)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ import duration from 'dayjs/plugin/duration';
 import timezone from 'dayjs/plugin/timezone';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 
+import { Metadata, ResolvingMetadata } from 'next';
+import { headers } from 'next/headers';
+
 export default function RootLayout({
 	children,
 }: {
@@ -39,4 +42,54 @@ export default function RootLayout({
 			</body>
 		</html>
 	);
+}
+
+export async function generateMetadata(
+	props: {
+		params: { id: string };
+		searchParams: { [key: string]: string | string[] | undefined };
+	},
+	parent?: ResolvingMetadata
+): Promise<Metadata> {
+	const headersList = headers();
+
+	const title = headersList.get('host')!;
+	const description = `Mallory — trans software engineer, game designer, & creator`;
+
+	return {
+		title,
+		description,
+		viewport: { width: 'device-width', initialScale: 1 },
+		openGraph: {
+			title,
+			description,
+			type: 'website',
+			url: `https://${title}`,
+		},
+		twitter: {
+			title,
+			description,
+			creator: '@_iGalaxyYT',
+		},
+		keywords: [
+			'iGalaxy',
+			'Mallory',
+			'Mallory Hayr',
+			'software engineer',
+			'game designer',
+			'creator',
+			'igalaxy.dev',
+			'mallory.rs',
+			'wii.mom',
+			'starship.mom',
+			'observatory.mom',
+		],
+		authors: [
+			{
+				name: 'Mallory Hayr',
+				url: 'https://igalaxy.dev',
+			},
+		],
+		themeColor: '#0A000A',
+	};
 }
