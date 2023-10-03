ed0751e25424751e54fec40e58882921568f403a
Mallory Hayr <hey@igalaxy.dev>
2023-10-02 19:55:49 -0700

    attempt to disable minification

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	compress: false,
+	webpack(webpackConfig) {
+		return {
+			...webpackConfig,
+			optimization: {
+				minimize: false,
+			},
+		};
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
