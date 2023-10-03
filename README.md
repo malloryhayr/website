cffc5daa4c6614a4e339756df3b4408087ec600f
Mallory Hayr <hey@igalaxy.dev>
2023-10-02 19:20:55 -0700

    fresh !

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 💻 website
 
-My personal website
+mallory's website
+
+available at:
+
+- https://igalaxy.dev
+- https://mallory.rs
+- https://wii.mom
--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	experimental: {
-		appDir: true,
-	},
-	reactStrictMode: false,
-	images: {
-		domains: [
-			'lastfm.freetls.fastly.net',
-			'cdn2.steamgriddb.com',
-			'steamcdn-a.akamaihd.net',
-		],
-	},
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
