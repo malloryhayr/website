2ce1e3f3fa5b00d4c729e337b52d1266127d945e
Mallory Hayr <hey@igalaxy.dev>
2023-11-16 15:21:13 -0800

    navbar stuff

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -2,7 +2,13 @@ export default function Navbar() {
 	return (
 		<nav>
 			<div>
-				<h1>igalaxy.dev</h1>
+				<h1>Mallory's observatory</h1>
+				<ul>
+					<li>home</li>
+					<li>shelf</li>
+					<li>projects</li>
+					<li>writing</li>
+				</ul>
 			</div>
 		</nav>
 	);
