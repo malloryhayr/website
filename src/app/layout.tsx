f2f8a87ec2d56318735af94a24616cd2e9c5dd8c
Mallory Hayr <hey@igalaxy.dev>
2023-11-23 03:38:15 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,12 @@ import type { Metadata, Viewport } from "next";
 import { headers } from "next/headers";
 
 import { Rubik } from "next/font/google";
-import localFont from "next/font/local";
 
 import "./(style)/global.scss";
 import Navbar from "./(components)/Navbar";
 import Footer from "./(components)/Footer";
 
-export const rubik = Rubik({ subsets: ["latin", "latin-ext"] });
+const rubik = Rubik({ subsets: ["latin", "latin-ext"] });
 
 export async function generateMetadata(): Promise<Metadata> {
 	const headersList = headers();
