94d48716463a3b0960a9973b1b15bc09250f5a75
Mallory Hayr <hey@igalaxy.dev>
2023-11-30 11:19:00 -0800

    Create page.tsx

---
+++ src/app/shelf/page.tsx
@@ -0,0 +1,9 @@
+export default async function Shelf() {
+	return (
+		<div style={{ width: "min(900px, 100vw)" }}>
+			<h2 style={{ color: "var(--color-header)" }}>music</h2>
+			<h2 style={{ color: "var(--color-header)" }}>games</h2>
+			<h2 style={{ color: "var(--color-header)" }}>movies</h2>
+		</div>
+	);
+}
