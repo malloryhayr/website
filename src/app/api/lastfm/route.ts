f9653d3881db5ff18f7dad358f4a5c74a5d355f3
Mallory Hayr <hey@igalaxy.dev>
2024-01-14 15:52:33 -0800

    fix caching

--- src/app/api/lastfm/route.ts
+++ src/app/api/lastfm/route.ts
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 export async function GET() {
 	const res = await fetch(
 		`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.LASTFM_USER}&api_key=${process.env.LASTFM_KEY}&format=json&limit=1`,
--- src/app/api/mastodon/route.ts
+++ src/app/api/mastodon/route.ts
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 export async function GET() {
 	const res = await fetch(
 		`https://farlands.cafe/api/v1/accounts/${process.env.MASTODON_USER}/statuses?limit=1&exclude_replies=true`,
